2b9ed02eb4aa91f43155ec0c463ce16c719481cd
Gabriel-Assis-22 <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-05-07 09:04:22 -0300

    commit

---
+++ desafio-2/src/pages/App.js
@@ -0,0 +1,12 @@
+import gitLogo from '../assets/gitLogo.png'
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src={gitLogo} width={72} height={72} />
+    </div>
+  );
+}
+
+export default App;
